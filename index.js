6b45b959939dcc564944609536a5977309c69945
Carlos Eduardo Pereira Santana <kaduridu@gmail.com>
2020-09-02 02:10:07 +0000

    Adição da classe Pessoa

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 //Aula 04//Modulos
 var modulo = require('./modules/modulos.js');
 modulo.modulo();
+//modularizando orientacao objeto
+const Pessoa = require('./modules/objetos/Pessoa.js')
+
+//Aula Orientacao Objeto
+
+const joao = new Pessoa(80,1.75); //instanciando um objeto
+console.log( joao.imc() ); 
+
+const pedro = new Pessoa();
+pedro.peso = 100;
+pedro.altura = 1.6;
+console.log( pedro.imc());
